7b639db5deb8c26647f7cd186893a77225027a5a
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-25 17:30:52 +0200

    [UPD]gestire almeno una Exception

--- index.php
+++ index.php
@@ -9,8 +9,12 @@ class Movie
 
     //creo una funzione col costruttore, per convenzione si mette in alto
     //le () simbooleggiano () in basso dove definisco le mie variabili
-    public function __construct(string $_title, string $_category, int $_vote)
+    public function __construct(string $_title, string $_category,  $_vote)
     {
+        if (!is_numeric($_vote)) {
+            throw new Exception('il voto devve essere un numero:' . $_vote);
+        }
+
         //cosi facendo assegnamo dei parametri $_ alle nostre istanze
         $this->title = $_title;
         $this->category = $_category;
@@ -42,25 +46,39 @@ class Movie
         $this->category = $categoria;
     }
     /*************/
-    public function get(): int
+    public function getVote(): int
     {
         return $this->vote;
     }
 
-    public function set(int $voto): void
+    public function setVote($voto): void
     {
+        //creao un eccezione
+        if (!is_numeric($voto)) {
+            throw new Exception('l\'aggiornamento voto devve essere un numero');
+        }
         $this->vote = $voto;
     }
 }
 
-//definisco le variabili d'istanza
-$movie1 = new Movie("Il Grande Film", "Documentary", 8);
-$movie2 = new Movie("La Grande Risata", "Comedy", 7);
-$movie3 = new Movie("V per Vuejs", "Action", 8);
-$movie4 = new Movie("The Lord of Ping", "Fantasy", 10);
+//fuori dalla classe
+try {
+    //istanze classe Movie
+    $movie1 = new Movie("Il Grande Film", "Documentary", 8);
+    $movie2 = new Movie("La Grande Risata", "Comedy", 10);
+    $movie3 = new Movie("V per Vuejs", "Action", 5);
+    $movie4 = new Movie("The Lord of Ping", "Fantasy", 10);
 
-
-var_dump($movie1);
-var_dump($movie2);
-var_dump($movie3);
-var_dump($movie4);
+    var_dump($movie1);
+    var_dump($movie2);
+    var_dump($movie3);
+    var_dump($movie4);
+    /*
+    quando si usa set 
+    $movie3->setVote(5);
+    echo $movie3->getVote();
+    */
+} catch (Exception $error) {
+    // var_dump($error);
+    echo '<h1 class="text-center">' . $error->getMessage() . '</h1>';
+}
